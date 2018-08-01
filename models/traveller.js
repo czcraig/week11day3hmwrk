85fa2a24378d413fdeb60215576b78c288f991c7
Chris Craig <ctc2@live.co.uk>
2018-08-01 16:22:15 +0100

    mvp done

---
+++ models/traveller.js
@@ -0,0 +1,40 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+  return journey.startLocation
+})
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+  return journey.endLocation
+})
+};
+
+Traveller.prototype.getModesOfTransport = function () {
+  return this.journeys.map((journey) => {
+  return journey.transport
+})
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter(journey => journey.transport === transport);
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter(journey => journey.distance > minDistance);
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
